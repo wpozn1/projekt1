aeeb8836d062ea7c3ebe3b53fc3b9541a12fb0c3
J-PRZ <przybyszjakub6@gmail.com>
2026-05-07 22:39:00 +0200

    refactor: added user system in backend

--- core_project/api/urls.py
+++ core_project/api/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import RandomMovieView
+from django.urls import path, include
+from .views import RandomMovieView, UserLibraryView
 
 urlpatterns = [
     path('match/', RandomMovieView.as_view(), name='movie_match'),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('library/', UserLibraryView.as_view(), name='user-library')
 ]
