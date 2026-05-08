81f94fa4c59db10d5273cec396608f9a309e7cd0
Mateusz Repeć <repec.mateusz@gmail.com>
2026-05-08 12:32:19 +0200

    chore: import changes v2 cause i dont know why this stopped working

--- core_project/api/views.py
+++ core_project/api/views.py
@@ -5,8 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from serializers import MovieSerializer
-from models import Movie
+from .serializers import MovieSerializer
+from .models import Movie
 
 
 class RandomMovieView(APIView):
