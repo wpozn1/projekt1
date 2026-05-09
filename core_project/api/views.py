162930aa6b09ff1dc42a606c13e241c6f009b9f9
Mateusz Repeć <repec.mateusz@gmail.com>
2026-05-09 13:37:29 +0200

    feat: added throttle_classes to views.py

--- core_project/api/views.py
+++ core_project/api/views.py
@@ -5,11 +5,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 from .serializers import MovieSerializer
 from .models import Movie
 
 
 class RandomMovieView(APIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+
     def post(self, request):
         genre_id=request.data.get('genre')
         provider_id=request.data.get('platform')
