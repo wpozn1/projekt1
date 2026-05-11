5729b60b0487844a257f971e16b1f0fde909ef6c
Mateusz Repeć <repec.mateusz@gmail.com>
2026-05-11 23:56:23 +0200

    chore: typo

--- core_project/core_project/settings.py
+++ core_project/core_project/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'core_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sglite:///{BASE_DIR / 'db.sqlite3'}",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600
     )
 }
